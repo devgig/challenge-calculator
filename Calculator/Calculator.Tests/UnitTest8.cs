746598346c60f93d0fabe5da0cc02af839f4e64c
LAPTOP-8MDRP3R0\geoff <geoff.niehaus@hotmail.com>
2019-08-21 12:52:27 -0500

    Step Eight

---
+++ Calculator/Calculator.Tests/UnitTest8.cs
@@ -0,0 +1,20 @@
+﻿using Calculator.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class UnitTest8
+    {
+        [Fact]
+        public void should_be_able_to_handle_multiple_custom_delimeters()
+        {
+            var engine = new CalculationProvider();
+            Assert.Equal(110, engine.Calculate(@"//[*][!!][rrr]\n11rrr22*33!!44"));
+
+        }
+
+    }
+}
